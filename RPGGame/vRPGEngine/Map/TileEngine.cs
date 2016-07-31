bb53ba06f44106ff5e9dc7052bf124741b621d50
babekz <niko.halikka@gmail.com>
2016-07-31 16:38:29 +0300

    add tile engine

---
+++ RPGGame/vRPGEngine/Map/TileEngine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGEngine.Map
+{
+    public static class TileEngine
+    {
+        #region Properties
+        public static int TileWidth
+        {
+            get;
+            private set;
+        }
+        public static int TileHeight
+        {
+            get;
+            private set;
+        }
+        public static int MapWidth
+        {
+            get;
+            private set;
+        }
+        public static int MapHeight
+        {
+            get;
+            private set;
+        }
+        #endregion
+
+        #region Events
+        public static event EventHandler PropertiesChanged;
+        #endregion
+
+        public static void ChangeProperties(int tileWidth, int tileHeight, int mapWidth, int mapHeight)
+        {
+            TileWidth   = tileWidth;
+            TileHeight  = tileHeight;
+
+            MapWidth    = mapWidth;
+            MapHeight   = mapHeight;
+
+            PropertiesChanged?.Invoke(null, EventArgs.Empty);
+        }
+    }
+}
