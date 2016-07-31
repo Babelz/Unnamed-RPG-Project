d1040bb85695dcd747cd3dd140828a6493ad8e33
babekz <niko.halikka@gmail.com>
2016-07-31 15:53:41 +0300

    add engine init

---
+++ RPGGame/vRPGEngine/Logger.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGEngine
+{
+    public enum LogLevel : int
+    {
+        Message = 0,
+        Warning,
+        Error
+    }
+
+    public interface ILogger
+    {
+        #region Properties
+        bool InUse
+        {
+            get;
+            set;
+        }
+        bool HasMessages
+        {
+            get;
+        }
+        #endregion
+
+        void Flush();
+
+        void Log(string message, LogLevel level);
+    }
+
+    public struct LogMessage
+    {
+        public LogLevel level;
+        public string contents;
+    }
+
+    public class LogFormatter
+    {
+        #region Fields
+        private readonly Func<string, string>[] formatters;
+        #endregion
+
+        public LogFormatter()
+        {
+        }
+
+        private string FormatMessage(string message)
+        {
+            return string.Format("[MSG]: {0}", message);
+        }
+        private string FormatWarning(string message)
+        {
+            return string.Format("[WRN]: {0}", message);
+        }
+        private string FormatError(string message)
+        {
+            return string.Format("[ERR]: {0}", message);
+        }
+
+        public string Format(string message, LogLevel level)
+        {
+            return formatters[(int)level](message);
+        }
+    }
+
+    public sealed class FileLogger : ILogger
+    {
+        #region Path
+        private static readonly string LogPath = "\\log.txt";
+        #endregion
+
+        #region Fields
+        private readonly List<LogMessage> buffer;
+
+        private readonly LogFormatter formatter;
+        #endregion
+
+        #region Properties
+        public bool InUse
+        {
+            get;
+            set;
+        }
+        public bool HasMessages
+        {
+            get
+            {
+                return buffer.Count != 0;
+            }
+        }
+        #endregion
+
+        public FileLogger()
+        {
+            buffer = new List<LogMessage>();
+
+            formatter = new LogFormatter();
+
+            if (!File.Exists(LogPath)) File.Create(LogPath);
+        }
+
+        public void Flush()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var message in buffer)
+            {
+                var formatted = formatter.Format(message.contents, message.level);
+
+                sb.Append(formatter);
+                sb.Append("\r\n");
+            }
+
+            File.WriteAllText(LogPath, sb.ToString());
+        }
+
+        public void Log(string message, LogLevel level)
+        {
+            LogMessage msg;
+            msg.contents    = message;
+            msg.level       = level;
+
+            buffer.Add(msg);
+        }
+    }
+
+    public sealed class ConsoleLogger : ILogger
+    {
+        #region Console colors
+        private static readonly ConsoleColor[] ConsoleColors = new ConsoleColor[]
+        {
+            ConsoleColor.Green,
+            ConsoleColor.Yellow,
+            ConsoleColor.Red
+        };
+        #endregion
+
+        #region Fields
+        private readonly List<LogMessage> buffer;
+
+        private readonly LogFormatter formatter;
+        #endregion
+
+        #region Properties
+        public bool InUse
+        {
+            get;
+            set;
+        }
+        public bool HasMessages
+        {
+            get
+            {
+                return buffer.Count != 0;
+            }
+        }
+        #endregion
+
+        public ConsoleLogger()
+        {
+            buffer = new List<LogMessage>();
+
+            formatter = new LogFormatter();
+        }
+
+        public void Flush()
+        {
+            foreach (var message in buffer)
+            {
+                var formatted   = formatter.Format(message.contents, message.level);
+                var color       = ConsoleColors[(int)message.level];
+
+                Console.ForegroundColor = color;
+
+                Console.WriteLine(formatted);
+
+                Console.ResetColor();
+            }
+            
+            buffer.Clear();
+        }
+
+        public void Log(string message, LogLevel level)
+        {
+            LogMessage msg;
+            msg.contents    = message;
+            msg.level       = level;
+
+            buffer.Add(msg);
+        }
+    }
+
+    public sealed class Logger : Singleton<Logger>
+    {
+        #region Fields
+        private readonly ILogger[] loggers;
+        #endregion
+
+        private Logger()
+            : base()
+        {
+            loggers = new ILogger[]
+            {
+                new FileLogger()    { InUse = true },
+                new ConsoleLogger() { InUse = true }
+            };
+        }
+        
+        public void LogFunctionMessage(string messge, [CallerLineNumber] int line = 0, [CallerMemberName] string fnc = null)
+        {
+            foreach (var logger in loggers.Where(l => l.InUse)) logger.Log(string.Format("{0}@{1} - {2}", fnc, line, messge), LogLevel.Message);
+        }
+        public void LogFunctionWarning(string messge, [CallerLineNumber] int line = 0, [CallerMemberName] string fnc = null)
+        {
+            foreach (var logger in loggers.Where(l => l.InUse)) logger.Log(string.Format("{0}@{1} - {2}", fnc, line, messge), LogLevel.Warning);
+        }
+        public void LogFunctionError(string messge, [CallerLineNumber] int line = 0, [CallerMemberName] string fnc = null)
+        {
+            foreach (var logger in loggers.Where(l => l.InUse)) logger.Log(string.Format("{0}@{1} - {2}", fnc, line, messge), LogLevel.Error);
+        }
+
+        public void LogMessage(string message)
+        {
+            foreach (var logger in loggers.Where(l => l.InUse)) logger.Log(message, LogLevel.Message);
+        }
+        public void LogWarning(string message)
+        {
+            foreach (var logger in loggers.Where(l => l.InUse)) logger.Log(message, LogLevel.Warning);
+        }
+        public void LogError(string message)
+        {
+            foreach (var logger in loggers.Where(l => l.InUse)) logger.Log(message, LogLevel.Error);
+        }
+
+        public void Flush()
+        {
+            foreach (var logger in loggers.Where(l => !l.HasMessages)) logger.Flush();
+        }
+    }
+}
---
+++ RPGGame/vRPGEngine/vRPGEngine.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGEngine
+{
+    public sealed class vRPGEngine : Singleton<vRPGEngine>
+    {
+        #region Fields
+        private Game game;
+        #endregion
+
+        #region Properties
+        public bool Running
+        {
+            get
+            {
+                return game.IsActive;
+            }
+        }
+        #endregion
+
+        private vRPGEngine() 
+            : base()
+        {
+        }
+
+        private void ActivateECS()
+        {
+            Logger.Instance.LogFunctionMessage("activating ECS...");
+        }
+        private void ActivateRenderingSystem()
+        {
+            Logger.Instance.LogFunctionMessage("activating render system...");
+        }
+        private void ActivateDataSystem()
+        {
+            Logger.Instance.LogFunctionMessage("activating data systems...");
+        }
+
+        public void InsertGame(Game game)
+        {
+            Debug.Assert(game != null);
+
+            this.game = game;
+        }
+
+        public void Start()
+        {
+            Debug.Assert(game != null);
+            Debug.Assert(!game.IsActive);
+
+            Logger.Instance.LogFunctionMessage("activating engine systems...");
+
+            try
+            {
+                ActivateECS();
+                ActivateRenderingSystem();
+                ActivateDataSystem();
+                
+                Logger.Instance.LogFunctionMessage("engine systems initialized ok!");
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.LogError("could not initialize all systems!");
+                Logger.Instance.LogError("and exceptin was thrown");
+                Logger.Instance.LogError("exception message: " + e.Message);
+
+                return;
+            }
+            
+            game.Run();
+        }
+
+        public void Exit()
+        {
+            game.Exit();
+        }
+    }
+}
