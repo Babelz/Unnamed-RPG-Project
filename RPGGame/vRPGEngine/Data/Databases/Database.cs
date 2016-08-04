1078a6a5b10b93504a3f676835bb6e411053f8cc
babekz <niko.halikka@gmail.com>
2016-08-04 10:43:56 +0300

    working with data sets

---
+++ RPGGame/vRPGEngine/Data/Databases/Database.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGEngine.Data.Databases
+{
+    public abstract class Database<T> : Singleton<Database<T>> where T : class
+    {
+        #region Fields
+        private readonly List<T> elements;
+        #endregion
+
+        #region Properties
+        public bool Readonly
+        {
+            get;
+            protected set;
+        }
+        #endregion
+
+        protected Database()
+            : base()
+        {
+            elements = LoadData();
+        }
+
+        protected abstract List<T> LoadData();
+
+        protected virtual void SaveData()
+        {
+        }
+
+        public T Query(Predicate<T> pred)
+        {
+            foreach (var element in elements) if (pred(element)) return element;
+
+            return null;
+        }
+
+        public void Sync()
+        {
+            if (Readonly) return;
+
+            SaveData();
+        }
+    }
+}
