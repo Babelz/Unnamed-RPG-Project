11339384db59e55b4135eeb63c7b0ca9bf34f5cc
babekz <niko.halikka@gmail.com>
2016-08-08 17:26:25 +0300

    add npc and factions

---
+++ RPGGame/vRPGContent/Data/Characters/Enums/Factions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGContent.Data.Characters.Enums
+{
+    [Flags()]
+    public enum Factions : int
+    {
+        /// <summary>
+        /// Not an actual faction, always hostile towards the player.
+        /// </summary>
+        WorldHostile = 1,
+
+        /// <summary>
+        /// Not an actual faction, always neutral towards the player.
+        /// </summary>
+        WorldNeutral = 2,
+        
+        /// <summary>
+        /// Not and actual faction, always friendly towards the player.
+        /// </summary>
+        WorldFriendly = 4
+    }
+}
