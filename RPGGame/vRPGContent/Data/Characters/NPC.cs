a30aa2f1345a55c1bbbd32509e6ce12f037f2cae
babekz <niko.halikka@gmail.com>
2016-08-08 17:32:56 +0300

    add npc and quest giver npc

--- RPGGame/vRPGContent/Data/Characters/NPC.cs
+++ RPGGame/vRPGContent/Data/Characters/NPC.cs
@@ -3,10 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using vRPGContent.Data.Characters.Enums;
 
 namespace vRPGContent.Data.Characters
 {
-    public sealed class NPC
+    [Serializable()]
+    public class NPC
     {
+        #region Properties
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int ID
+        {
+            get;
+            set;
+        }
+        public int Health
+        {
+            get;
+            set;
+        }
+        public int Mana
+        {
+            get;
+            set;
+        }
+        public int Focus
+        {
+            get;
+            set;
+        }
+        public int Level
+        {
+            get;
+            set;
+        }
+        public int MeleePower
+        {
+            get;
+            set;
+        }
+        public int SpellPower
+        {
+            get;
+            set;
+        }
+        public float CritChance
+        {
+            get;
+            set;
+        }
+        public Factions Faction
+        {
+            get;
+            set;
+        }
+        public int[] LootList
+        {
+            get;
+            set;
+        }
+        public int[] SpellList
+        {
+            get;
+            set;
+        }
+        public int[] SpellPriority
+        {
+            get;
+            set;
+        }
+        public string HandlerName
+        {
+            get;
+            set;
+        }
+        public string TextureSetName
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        public NPC()
+        {
+        }
     }
 }
---
+++ RPGGame/vRPGContent/Data/Characters/NPCQuestGiver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGContent.Data.Characters
+{
+    [Serializable()]
+    public sealed class NPCQuestGiver : NPC
+    {
+        #region Properties
+        public int[] Quests
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        NPCQuestGiver()
+            : base()
+        {
+        }
+    }
+}
