adc5f223fcad3e3773e02022d61a8fbb85d2791a
babekz <niko.halikka@gmail.com>
2016-08-08 17:36:27 +0300

    add quests

---
+++ RPGGame/vRPGContent/Data/Quests/Enums/QuestType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGContent.Data.Quests.Enums
+{
+    public enum QuestType : int
+    {
+        Normal = 1,
+        Daily,
+        Repeatable
+    }
+}
---
+++ RPGGame/vRPGContent/Data/Quests/Quest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vRPGContent.Data.Quests.Enums;
+
+namespace vRPGContent.Data.Quests
+{
+    [Serializable()]
+    public sealed class Quest
+    {
+        #region Fields
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int ID
+        {
+            get;
+            set;
+        }
+        public QuestType Type
+        {
+            get;
+            set;
+        }
+        public string[] Description
+        {
+            get;
+            set;
+        }
+        public int[] ItemRewards
+        {
+            get;
+            set;
+        }
+        public int MoneyReward
+        {
+            get;
+            set;
+        }
+        public string HandlerName
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        public Quest()
+        {
+        }
+    }
+}
