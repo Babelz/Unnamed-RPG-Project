cafe0dd5f837530e4064997f3e5741eb7389e4f8
babekz <niko.halikka@gmail.com>
2016-08-14 14:57:56 +0300

    add scenes

---
+++ RPGGame/vRPGEngine/Scenes/Scene.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vRPGEngine.ECS;
+
+namespace vRPGEngine.Scenes
+{
+    public abstract class Scene
+    {
+        #region Fields
+        private readonly List<Entity> entitites;
+        #endregion
+
+        #region Properties
+        protected IEnumerable<Entity> Entitites
+        {
+            get
+            {
+                return entitites;
+            }
+        }
+        #endregion
+
+        public Scene()
+        {
+            entitites = new List<Entity>();
+        }
+
+        protected void Register(Entity entity)
+        {
+            entitites.Add(entity);
+        }
+        protected void Unregister(Entity entity)
+        {
+            entitites.Add(entity);
+        }
+
+        public virtual void Initialize()
+        {
+        }
+        public virtual void Deinitialize()
+        {
+        }
+        
+        public virtual bool ChangeScene()
+        {
+            return true;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+    }
+}
---
+++ RPGGame/vRPGEngine/Scenes/SceneManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace vRPGEngine.Scenes
+{
+    public sealed class SceneManager : SystemManager<SceneManager>
+    {
+        #region Fields
+        private bool sceneChanging;
+
+        private Scene current;
+        private Scene next;
+        #endregion
+
+        private SceneManager()
+            : base()
+        {
+        }
+
+        public void ChangeScene(Scene scene)
+        {
+            next = scene;
+
+            next.Initialize();
+        }
+
+        protected override void OnUpdate(GameTime gameTime)
+        {
+            if (sceneChanging)
+            {
+                if (next.ChangeScene())
+                {
+                    if (current != null) current.Deinitialize();
+
+                    sceneChanging   = false;
+                    current         = next;
+                    next            = null;
+                }
+            }
+
+            if (current != null) current.Update(gameTime);
+            if (next != null)    next.Update(gameTime);
+        }
+    }
+}
