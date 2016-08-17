6d503ae03e51fbd1ea6d1807bba851e92e2081aa
babekz <niko.halikka@gmail.com>
2016-08-17 20:00:23 +0300

    working with buff and spell icons

---
+++ RPGGame/vRPGEngine/HUD/Controls/BindButton.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vRPGContent.Data.Spells;
+using vRPGEngine.ECS;
+using vRPGEngine.ECS.Components;
+using vRPGEngine.HUD.Elements;
+using vRPGEngine.Input;
+using Microsoft.Xna.Framework;
+using vRPGEngine.Handlers.Spells;
+
+namespace vRPGEngine.HUD.Controls
+{
+    public sealed class BindButton : ButtonBase
+    {
+        #region Fields
+        private readonly string name;
+
+        private IDisplayElement element;
+
+        private SpellHandler handler;
+
+        private Spell spell;
+        #endregion
+
+        #region Properties
+        public IDisplayElement Element
+        {
+            get
+            {
+                return element;
+            }
+            set
+            {
+                element = value;
+
+                NotifyPropertyChanged("Spell");
+            }
+        }
+
+        public Spell Spell
+        {
+            get
+            {
+                return spell;
+            }
+            set
+            {
+                spell = value;
+
+                NotifyPropertyChanged("Spell");
+            }
+        }
+
+        private SpellHandler Handler
+        {
+            get
+            {
+                return handler;
+            }
+            set
+            {
+                handler = value;
+
+                NotifyPropertyChanged("Handler");
+            }
+        }
+        #endregion
+
+        public BindButton(string name)
+            : base()
+        {
+            this.name = name;
+
+            ButtonPressed   += BindButton_ButtonPressed;
+            PropertyChanged += BindButton_PropertyChanged;
+        }
+
+        #region Event handlers
+        private void BindButton_PropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+            Invalidate();
+        }
+        private void BindButton_ButtonPressed(Interfaces.IButtonControl sender)
+        {
+            Use();
+        }
+        #endregion
+
+        protected override void OnInvalidate()
+        {
+            var player = EntityManager.Instance.Entitites.First(e => e.Tags == "player");
+
+            var controller = player.FirstComponentOfType<PlayerCharacterController>();
+
+            if (spell != null) Handler = controller.Spells.FirstOrDefault(p => p.Spell.ID == spell.ID);
+            
+            if (element != null) element.Invalidate(this);
+        }
+
+        private void Use()
+        {
+            var player = EntityManager.Instance.Entitites.First(e => e.Tags == "player");
+            
+            if (Handler != null) Handler.Use(player);
+        }
+
+        public void SetTrigger(Keys key)
+        {
+            var kb = InputManager.Instance.GetProvider<KeyboardInputProvider>();
+
+            kb.Unbind(name);
+
+            kb.Bind(name, key, KeyTrigger.Pressed, () =>
+            {
+                Use();
+            });
+
+            Invalidate();
+        }
+    }
+}
---
+++ RPGGame/vRPGEngine/HUD/Elements/BindButtonElement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using vRPGEngine.HUD.Controls;
+using vRPGEngine.Handlers.Spells;
+using vRPGContent.Data.Spells;
+
+namespace vRPGEngine.HUD.Elements
+{
+    public sealed class BindButtonElement : IDisplayElement
+    {
+        #region Fields
+        private Vector2 position;
+        private Vector2 scale;
+
+        private MouseHoverState hoverState;
+        
+        private SpellHandler handler;
+        private Spell spell;
+        #endregion
+
+        public BindButtonElement()
+        {
+        }
+
+        public void Invalidate(Control control)
+        {
+            if (!control.ReadProperty("Spell", ref spell)) return;
+
+            if (!control.ReadProperty("Handler", ref handler)) spell = null;
+
+            position = control.DisplayPosition;
+            scale    = control.DisplaySize / 32.0f;
+        }
+
+        public void Show(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (spell != null)
+            {
+                spriteBatch.Draw(Engine.Instance.Content.Load<Texture2D>(spell.IconName),
+                                 position,
+                                 null,
+                                 null,
+                                 null,
+                                 0.0f,
+                                 scale,
+                                 Color.White,
+                                 SpriteEffects.None,
+                                 0.0f);
+
+                if (hoverState == MouseHoverState.Hover)
+                {
+                    // Draw info.    
+                }
+            }
+        }
+    }
+}
