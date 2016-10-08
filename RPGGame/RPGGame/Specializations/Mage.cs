40a25b2f0678fc067b6919ea429b5ef4ec2f3092
Babelz <niko.halikka@gmail.com>
2016-10-08 15:46:44 +0300

    fix my brain farting w mage spec impl location

--- RPGGame/RPGGame/Specializations/Mage.cs
+++ RPGGame/RPGGame/Specializations/Mage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using vRPGContent.Data.Attributes;
+using vRPGContent.Data.Items.Enums;
 using vRPGEngine.Attributes;
 using vRPGEngine.Databases;
 using vRPGEngine.Specializations;
@@ -12,10 +13,62 @@ namespace RPGGame.Specializations
 {
     public sealed class Mage : Specialization
     {
-        public Mage(AttributesData attributes, Statuses statuses) 
+        #region Fields
+        private EquipmentContainer equipments;
+        #endregion
+
+        public Mage(EquipmentContainer equipments, AttributesData attributes, Statuses statuses) 
             : base(SpecializationDatabase.Instance.Elements().First(e => e.Name == "mage"), attributes, statuses)
         {
-            
+            this.equipments = equipments;
+        }
+
+         private bool IsWearingFullCloth()
+        {
+            return equipments.EquipedArmorsCount == equipments.Armors.Count(a => a.ArmorType == ArmorType.Cloth);
+        }
+        private bool IsWearingFullLeather()
+        {
+            return equipments.EquipedArmorsCount == equipments.Armors.Count(a => a.ArmorType == ArmorType.Leather);
+        }
+
+        public override float TotalCriticalHitPercent()
+        {
+            return base.TotalCriticalHitPercent();
+        }
+        
+        public override int TotalSpellPower()
+        {
+            if (IsWearingFullCloth()) return (int)(base.TotalSpellPower() * 1.1f);
+
+            return base.TotalSpellPower();
+        }
+        public override int TotalIntellect()
+        {
+            var mul = 1.0f;
+
+            if      (IsWearingFullCloth())      mul = 1.1f;
+            else if (IsWearingFullLeather())    mul = 1.05f;
+
+            return (int)(base.TotalIntellect() * mul);
+        }
+        public override int TotalStamina()
+        {
+            if (IsWearingFullLeather()) return (int)(base.TotalStamina() * 1.1f);
+
+            return base.TotalStamina();
+        }
+        public override float TotalMovementSpeedPercent()
+        {
+            if (IsWearingFullCloth()) return base.TotalMovementSpeedPercent() + 0.05f;
+
+            return base.TotalMovementSpeedPercent();
+        }
+        public override int TotalHp5()
+        {
+            if (IsWearingFullLeather()) return base.TotalHp5() + Attributes.Level;
+
+            return base.TotalHp5();
         }
     }
 }
--- RPGGame/vRPGEngine/Attributes/Specializations/DefaultNPCSpecialization.cs
+++ RPGGame/vRPGEngine/Attributes/Specializations/DefaultNPCSpecialization.cs
@@ -12,14 +12,9 @@ namespace vRPGEngine.Attributes.Specializations
 {
     public sealed class DefaultNPCSpecialization : Specialization
     {
-        #region Fields
-        private EquipmentContainer equipments;
-        #endregion
-
-        public DefaultNPCSpecialization(EquipmentContainer equipments, AttributesData attributes, Statuses statuses)
+        public DefaultNPCSpecialization(AttributesData attributes, Statuses statuses)
             : base(SpecializationDatabase.Instance.Elements().First(e => e.Name.ToLower() == "npc default"), attributes, statuses)
         {
-            this.equipments = equipments;
         }
 
         protected override void SetRations(ref int staminaToHealthRation, ref int intellectToManaRation, ref int enduranceToFocusRation)
@@ -27,54 +22,6 @@ namespace vRPGEngine.Attributes.Specializations
             staminaToHealthRation  = DefaultStaminaToHealthRation;
             intellectToManaRation  = DefaultIntellectToManaRation;
             enduranceToFocusRation = DefaultEnduranceToFocusRation;
-        }
-
-        private bool IsWearingFullCloth()
-        {
-            return equipments.EquipedArmorsCount == equipments.Armors.Count(a => a.ArmorType == ArmorType.Cloth);
-        }
-        private bool IsWearingFullLeather()
-        {
-            return equipments.EquipedArmorsCount == equipments.Armors.Count(a => a.ArmorType == ArmorType.Leather);
-        }
-
-        public override float TotalCriticalHitPercent()
-        {
-            return base.TotalCriticalHitPercent();
-        }
-        
-        public override int TotalSpellPower()
-        {
-            if (IsWearingFullCloth()) return (int)(base.TotalSpellPower() * 1.1f);
-
-            return base.TotalSpellPower();
-        }
-        public override int TotalIntellect()
-        {
-            var mul = 1.0f;
-
-            if      (IsWearingFullCloth())      mul = 1.1f;
-            else if (IsWearingFullLeather())    mul = 1.05f;
-
-            return (int)(base.TotalIntellect() * mul);
-        }
-        public override int TotalStamina()
-        {
-            if (IsWearingFullLeather()) return (int)(base.TotalStamina() * 1.1f);
-
-            return base.TotalStamina();
-        }
-        public override float TotalMovementSpeedPercent()
-        {
-            if (IsWearingFullCloth()) return base.TotalMovementSpeedPercent() + 0.05f;
-
-            return base.TotalMovementSpeedPercent();
-        }
-        public override int TotalHp5()
-        {
-            if (IsWearingFullLeather()) return base.TotalHp5() + Attributes.Level;
-
-            return base.TotalHp5();
         }
     }
 }
