3343813c35135ad423b0efef3fecf22fe1e9f9df
babelz <niko.halikka@gmail.com>
2016-10-24 07:33:04 +0300

    add files

--- RPGGame/RPGGame/Handlers/Spells/AutoAttack.cs
+++ RPGGame/RPGGame/Handlers/Spells/AutoAttack.cs
@@ -14,35 +14,35 @@ using vRPGEngine.Handlers.Spells;
 
 namespace RPGGame.Handlers.Spells
 {
-    //public sealed class AutoAttack : MeleeSpellHandler
-    //{
-    //    public AutoAttack() 
-    //        : base("auto attack")
-    //    {
-    //    }
-        
-    //    protected override MeleeSpellState OnUse(GameTime gameTime)
-    //    {
-    //        if (UserController.TargetFinder.Target == null)         return MeleeSpellState.Inactive;
-    //        if (!SpellHelper.InRange(UserController, User, Spell))  return MeleeSpellState.Inactive;
+    public sealed class AutoAttack : MeleeSpellHandler
+    {
+        public AutoAttack()
+            : base("auto attack")
+        {
+        }
 
-    //        foreach (var swing in UserController.MeleeDamageController.Results())
-    //        {
-    //            var damage = (int)(swing.Damage + UserController.Specialization.TotalAttackPower() * 0.25f);
+        protected override MeleeSpellState OnUse(GameTime gameTime)
+        {
+            if (UserController.TargetFinder.Target == null) return MeleeSpellState.Inactive;
+            if (!SpellHelper.InRange(UserController, User, Spell)) return MeleeSpellState.Inactive;
 
-    //            UserController.TargetFinder.TargetController.Statuses.Health -= damage;
+            foreach (var swing in UserController.MeleeDamageController.Results())
+            {
+                var damage = (int)(swing.Damage + UserController.Specialization.TotalAttackPower() * 0.25f);
 
-    //            GameInfoLog.Instance.LogDealDamage(damage, swing.Critical, Spell.Name, UserController.TargetFinder.TargetController.Name);
+                UserController.TargetFinder.TargetController.Statuses.Health -= damage;
 
-    //            if (!UserController.TargetFinder.TargetController.Statuses.Alive) return MeleeSpellState.Inactive;
-    //        }
+                GameInfoLog.Instance.LogDealDamage(damage, swing.Critical, Spell.Name, UserController.TargetFinder.TargetController.Name);
 
-    //        return MeleeSpellState.Active;
-    //    }
+                if (!UserController.TargetFinder.TargetController.Statuses.Alive) return MeleeSpellState.Inactive;
+            }
 
-    //    public override SpellHandler Clone()
-    //    {
-    //        return new AutoAttack();
-    //    }
-    //}
+            return MeleeSpellState.Active;
+        }
+
+        public override SpellHandler Clone()
+        {
+            return new AutoAttack();
+        }
+    }
 }
---
+++ RPGGame/vRPGEngine/Handlers/Spells/MeleeSpellHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vRPGEngine.Handlers.Spells
+{
+    public abstract class MeleeSpellHandler
+    {
+        #region Fields
+
+        #endregion
+    }
+}
